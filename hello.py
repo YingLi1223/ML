8e4437918d695ad736b2076a7dfa067a66f6104b
YingLi1223 <1045236103@qq.com>
2025-04-02 20:55:12 +0200

    更新内容

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello Git!")
+import torch
+import matplotlib.pyplot as plt
+
+x = torch.linspace(-5, 5, 200)
+y = torch.tanh(x)
+
+plt.plot(x, y)
+plt.title("Tanh Activation Function")
+plt.xlabel("Input")
+plt.ylabel("Output")
+plt.grid(True)
+plt.show()
+
